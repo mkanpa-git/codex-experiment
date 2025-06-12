437b11832f913eafdc89b12c2003e062a6a41fd7
mkanpa-git <79177729+mkanpa-git@users.noreply.github.com>
2025-06-12 00:04:29 -0400

    feat(accessibility): improve stepper and tooltip aria attributes

--- childcare-app/components/fields/Tooltip.tsx
+++ childcare-app/components/fields/Tooltip.tsx
@@ -14,10 +14,15 @@ export default function Tooltip({ content, children }: TooltipProps) {
       onMouseLeave={() => setOpen(false)}
       onFocus={() => setOpen(true)}
       onBlur={() => setOpen(false)}
+      tabIndex={0}
+      aria-label={content}
     >
       {children}
       {open && (
-        <span className="absolute left-1/2 -translate-x-1/2 mt-1 whitespace-nowrap bg-black text-white text-xs rounded py-1 px-2 z-10">
+        <span
+          role="tooltip"
+          className="absolute left-1/2 -translate-x-1/2 mt-1 whitespace-nowrap bg-black text-white text-xs rounded py-1 px-2 z-10"
+        >
           {content}
         </span>
       )}
